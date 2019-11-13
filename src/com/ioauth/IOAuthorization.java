19fa0de0f78e7b1b1784f9c8c3279d496bd5c2d9
Orion <ocrocker@pdx.edu>
2019-11-12 23:20:23 -0800

    basic io length check

---
+++ src/com/ioauth/IOAuthorization.java
@@ -0,0 +1,9 @@
+
+
+public class IOAuthorization {
+
+    // check if input is size of expected length
+    public boolean checkInputSize(String input, int expected) {
+        return input.length() == expected;
+    }
+}
--- src/com/ioauth/Main.java
+++ src/com/ioauth/Main.java
@@ -3,6 +3,20 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("This is a test.");
+        /* Main function,
+           Will primarily be used for tests
+         */
+
+        Scanner keyboard = new Scanner(System.in);
+        IOAuthorization IO = new IOAuthorization();
+
+        // get user input
+        System.out.print("Enter an 8 digit user number\n-> ");
+        String userInput = keyboard.nextLine();
+
+        if(IO.checkInputSize(userInput, 8))
+            System.out.println("Success!");
+        else
+            System.out.println("Failure");
     }
 }
