c74780a1577d7f8c2b17d76154196804eead757f
orion <orioncrocker@gmail.com>
2019-11-13 18:23:26 -0800

    Added isInputNumber and isType with loop for testing. Need to write comprehensive tests.

--- src/com/ioauth/IOAuthorization.java
+++ src/com/ioauth/IOAuthorization.java
@@ -1,9 +1,24 @@
-
-
 public class IOAuthorization {
 
     // check if input is size of expected length
     public boolean checkInputSize(String input, int expected) {
         return input.length() == expected;
     }
+
+    public boolean isInputNumber(String input) {
+        return this.isType(input, "int");
+    }
+
+	public Boolean isType(String testStr, String type) {
+		try {
+			if (type.equalsIgnoreCase("int")) {
+				Integer.parseInt(testStr);
+			} else if (type.equalsIgnoreCase("double")) {
+				Double.parseDouble(testStr);
+			}
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
 }
--- src/com/ioauth/Main.java
+++ src/com/ioauth/Main.java
@@ -1,22 +1,30 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         /* Main function,
            Will primarily be used for tests
          */
-
         Scanner keyboard = new Scanner(System.in);
         IOAuthorization IO = new IOAuthorization();
 
         // get user input
-        System.out.print("Enter an 8 digit user number\n-> ");
+
+        System.out.print("Enter an 8 digit user number or q to quit.\n>");
         String userInput = keyboard.nextLine();
 
-        if(IO.checkInputSize(userInput, 8))
-            System.out.println("Success!");
-        else
-            System.out.println("Failure");
+        while(!userInput.equalsIgnoreCase("q")) {
+
+            if (IO.checkInputSize(userInput, 8))
+                if (IO.isInputNumber(userInput))
+                    System.out.println("Success!");
+                else
+                    System.out.println("User ID must be a number, no characters.");
+            else
+                System.out.println("User ID must be 8 digits long.");
+
+            System.out.print("Enter an 8 digit user number or q to quit.\n>");
+            userInput = keyboard.nextLine();
+        }
     }
 }
