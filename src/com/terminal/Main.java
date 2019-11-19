5db420cae9a55227e991dc2ae461ca9f5ef4accb
Orion <ocrocker@pdx.edu>
2019-11-18 19:39:35 -0800

    "Merged" ioauth class with terminal. Everything is tested and should be ready to use.

---
+++ src/com/terminal/Main.java
@@ -0,0 +1,11 @@
+package com.terminal;
+
+public class Main
+{
+    public static void main (String[] args)
+    {
+        System.out.print("Hello World.");
+
+        IOAuthorization IO = new IOAuthorization();
+    }
+}
