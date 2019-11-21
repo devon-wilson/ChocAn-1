17cd2d48ca59ce2f778597dec6ac12f7668ba530
Jason <Jason.Forral@gmail.com>
2019-11-20 16:21:04 -0800

    Added more blank functions to Overlord

--- src/com/overlord/Main.java
+++ src/com/overlord/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        Overlord overlord = new Overlord();
+        int rc = overlord.loginUser("0", "0");
     }
 }
--- src/com/overlord/Overlord.java
+++ src/com/overlord/Overlord.java
@@ -11,21 +11,68 @@
 public class Overlord {
     public static void main(String[] args) {
         System.out.println("Overlord tests");
-        //todo test generate report
-        //todo test generate service record
+        Overlord overlord = new Overlord();
+        int rc = overlord.loginUser("0", "0");
     }
 
-    //todo create and test current user
-    //todo create and test current member
-
-    public void loginUser(String a, String b) {
+    // should return User object
+    public int loginUser(String providerID, String otherID) {
+        return 0;
     }
+
+    // Member functions
+    public int memberCheckIn() { return 0;}
 
     public int generateServiceRecord() {
         return 0;
     }
 
-    public int generateReport(String input) {
+    public int requestDirectory() { return 0; }
+
+    public int generateBill() {return 0;}
+
+    public int exit() {return 0;}
+
+    // Manage members
+    public int addMember(int MEMBER_PLACEHOLDER) { return 0; }
+
+    public int removeMember(int memberID) {return 0;}
+
+    public int suspendMember(int memberID) {return 0;}
+
+    public int renewMember(int memberID) {return 0;}
+
+    // should return Member object
+    public void searchMember(String query) {}
+
+
+    // Manage providers
+    public int addProvider(int PROVIDER_PLACEHOLDER) { return 0; }
+
+    public int removeProvider(int providerID) {return 0;}
+
+    // should return Provider object
+    public void searchProvider(String query) {}
+
+    // Manage services
+    public int addService(int SERVICE_PLACEHOLDER) { return 0; }
+
+    public int removeService(int serviceID) {return 0;}
+
+    // should return Service object
+    public int searchService(String query) {return 0;}
+
+    public int genMemberReport(int memberID) {return 0;}
+
+    public int genProviderReport(int memberID) {
+        return 0;
+    }
+
+    public int genAllMemberReports() {
+        return 0;
+    }
+
+    public int genAllProvidersReports() {
         return 0;
     }
 
