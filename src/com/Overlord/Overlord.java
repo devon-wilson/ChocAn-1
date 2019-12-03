9188321a1bab4b9ffd4b27469a574418c9820b38
Jason <Jason.Forral@gmail.com>
2019-12-02 22:20:45 -0800

    change spacing and cleaned some things

--- src/com/Overlord/Overlord.java
+++ src/com/Overlord/Overlord.java
@@ -14,285 +14,281 @@ import com.DataClasses.*;
  * @version 1.0.0
  */
 public class Overlord extends DataBaseManager {
-    public static void main(String[] args) {
-        //Overlord_Tests.main(null);
-    }
+  private User currentUser;
+  private Member currentMember;
 
-    private User currentUser;
-    private Member currentMember;
+  /**
+   * Default Constructor
+   */
+  public Overlord() {
+    // first argument is filepath to
+    //super(arguments);
+    super();
+    // second argument is..
 
-    /**
-     * Default Constructor
-     */
-    public Overlord() {
-       // first argument is filepath to
-       //super(arguments);
-        super();
-       // second argument is..
+    this.currentUser = null;
+    this.currentMember = null;
+  }
 
-        this.currentUser = null;
-        this.currentMember = null;
-    }
+  /**
+   * Login
+   *
+   * @param userID Provider identification number
+   * @return returns int: 1 for provider; 2 for manager; -1 could not log in
+   */
+  public int login(int userType, String userID) {
+    // matches string to provider directory
+    // if no match return -1
 
-    /**
-     * Login
-     *
-     * @param userID Provider identification number
-     * @return returns int: 1 for provider; 2 for manager; -1 could not log in
-     */
-    public int login(int userType, String userID) {
-        // matches string to provider directory
-        // if no match return -1
+    // userType must be either 0 or 1
+    if (userType < 0 || userType > 1)
+      return -1;
 
-        // userType must be either 0 or 1
-        if (userType < 0 || userType > 1)
-            return -1;
-
-        try {
-            User login = (User) findData(userType, userID);
-            if (login == null)
-                return -1;
-
-            // sets currentUser
-            currentUser = login;
-            return 1;
-        }
-        catch (ClassCastException a){
-            return -1;
-        }
-    }
-
-    /**
-     * Logout
-     *
-     * @return 1 and cannot fail.
-     */
-    public int logout() {
-        currentUser = null;
-        currentMember = null;
+    try {
+      User login = (User) findData(userType, userID);
+      if (login == null)
         return -1;
+
+      // sets currentUser
+      currentUser = login;
+      return 1;
     }
-
-    // Member functions
-
-    /**
-     * Member Check In
-     * @param memberID contains id of member
-     * @return returns 1 for success, -1 for failure.
-     */
-    public int memberCheckIn(String memberID) {
-        // match string to member directory
-
-        try {
-            this.currentMember = (Member) findData(2, memberID);
-            if (currentMember == null)
-                return -1;
-            return 1;
-        }
-        catch (ClassCastException a) {
-            this.currentMember = null;
-            return -1;
-        }
+    catch (ClassCastException a){
+      return -1;
     }
+  }
 
-    public void viewMember() {
-        if (currentMember == null)
-            System.out.println("No member current checked in.");
-        else
-            currentMember.display();
+  /**
+   * Logout
+   *
+   * @return 1 and cannot fail.
+   */
+  public int logout() {
+    currentUser = null;
+    currentMember = null;
+    return -1;
+  }
+
+  // Member functions
+
+  /**
+   * Member Check In
+   * @param memberID contains id of member
+   * @return returns 1 for success, -1 for failure.
+   */
+  public int memberCheckIn(String memberID) {
+    // match string to member directory
+
+    try {
+      this.currentMember = (Member) findData(2, memberID);
+      if (currentMember == null)
+        return -1;
+      return 1;
     }
-
-    /**
-     * Member Check Out
-     * @return 1 for success, -1 for failure.
-     */
-    public int memberCheckOut() {
-        // Check if member is checked in
-        if(currentMember == null)
-            return -1;
-        // Set current member to null
-        this.currentMember = null;
-        return 1;
+    catch (ClassCastException a) {
+      this.currentMember = null;
+      return -1;
     }
+  }
 
-    // Manage members
+  public void viewMember() {
+    if (currentMember == null)
+      System.out.println("No member current checked in.");
+    else
+      currentMember.display();
+  }
 
-    /**
-     * Add Member
-     *
-     * @param memberData Array of strings containing data in the correct order.
-     * @return 1 for success, -1 for failure
-     */
-    public int addMember(String[] memberData) {
-        Member toAdd = new Member(memberData);
+  /**
+   * Member Check Out
+   * @return 1 for success, -1 for failure.
+   */
+  public int memberCheckOut() {
+    // Check if member is checked in
+    if(currentMember == null)
+      return -1;
+    // Set current member to null
+    this.currentMember = null;
+    return 1;
+  }
 
-        if(addTreeData(2, toAdd.get(1), toAdd) == null)
-            return -1;
-        return 1;
+  // Manage members
+
+  /**
+   * Add Member
+   *
+   * @param memberData Array of strings containing data in the correct order.
+   * @return 1 for success, -1 for failure
+   */
+  public int addMember(String[] memberData) {
+    Member toAdd = new Member(memberData);
+
+    if(addTreeData(2, toAdd.get(1), toAdd) == null)
+      return -1;
+    return 1;
+  }
+
+  /**
+   * RemoveMember
+   *
+   * @param memberID member ID to remove
+   * @return
+   */
+  public int removeMember(String memberID) {
+    Object removed = removeTreeData(2, memberID);
+    if (removed == null)
+      return -1;
+    return 1;
+  }
+
+  public int suspendMember(String memberID) {
+    try {
+      // check if member to be suspended is current member
+      if (currentMember != null && currentMember.get(1).equals(memberID)) {
+        currentMember.updateStatus("SUSPENDED");
+        // update tree with current info
+        updateTreeData(2, memberID, currentMember);
+      }
+
+      else {
+        // pull info from tree
+        Member toUpdate = (Member) findData(2, memberID);
+        toUpdate.updateStatus("SUSPENDED");
+        // put info back on tree
+        updateTreeData(2, memberID, toUpdate);
+      }
+      return 1;
     }
-
-    /**
-     * RemoveMember
-     *
-     * @param memberID member ID to remove
-     * @return
-     */
-    public int removeMember(String memberID) {
-        Object removed = removeTreeData(2, memberID);
-        if (removed == null)
-            return -1;
-        return 1;
+    catch (ClassCastException a) {
+      return -1;
     }
+  }
 
-    public int suspendMember(String memberID) {
-        try {
-            // check if member to be suspended is current member
-            if (currentMember != null && currentMember.get(1).equals(memberID)) {
-                currentMember.updateStatus("SUSPENDED");
-                // update tree with current info
-                updateTreeData(2, memberID, currentMember);
-            }
+  public int renewMember(String memberID) {
+    try {
+      if (currentMember != null && currentMember.get(1).equals(memberID)) {
+        currentMember.updateStatus("SUSPENDED");
+        // update tree with current info
+        updateTreeData(2, memberID, currentMember);
+      }
+      else {
+        // pull info from tree
+        Member toUpdate = (Member) findData(2, memberID);
+        toUpdate.updateStatus("Validated");
 
-            else {
-                // pull info from tree
-                Member toUpdate = (Member) findData(2, memberID);
-                toUpdate.updateStatus("SUSPENDED");
-                // put info back on tree
-                updateTreeData(2, memberID, toUpdate);
-            }
-            return 1;
-        }
-        catch (ClassCastException a) {
-            return -1;
-        }
+        // put info back on tree
+        updateTreeData(2, memberID, toUpdate);
+      }
+      return 1;
     }
-
-    public int renewMember(String memberID) {
-        try {
-            if (currentMember != null && currentMember.get(1).equals(memberID)) {
-                currentMember.updateStatus("SUSPENDED");
-                // update tree with current info
-                updateTreeData(2, memberID, currentMember);
-            }
-            else {
-                // pull info from tree
-                Member toUpdate = (Member) findData(2, memberID);
-                toUpdate.updateStatus("Validated");
-
-                // put info back on tree
-                updateTreeData(2, memberID, toUpdate);
-            }
-            return 1;
-        }
-        catch (ClassCastException a) {
-            return -1;
-        }
+    catch (ClassCastException a) {
+      return -1;
     }
+  }
 
-    // should return Member object
-    public Member searchMember(String query) {
-        try {
-            return (Member) findData(2, query);
-        }
-        catch (ClassCastException a) {
-            return null;
-        }
+  // should return Member object
+  public Member searchMember(String query) {
+    try {
+      return (Member) findData(2, query);
     }
-
-    // Manage providers
-    public int addProvider(String[] providerData) {
-        Provider toAdd = new Provider(providerData);
-        if (providerData.length < 2)
-            return -1;
-        if(addTreeData(1, providerData[1], toAdd) == null)
-            return -1;
-        return 1;
+    catch (ClassCastException a) {
+      return null;
     }
+  }
 
-    public int removeProvider(String providerID) {
-        if(removeTreeData(1,providerID) == null)
-            return -1;
-        return 1;
+  // Manage providers
+  public int addProvider(String[] providerData) {
+    Provider toAdd = new Provider(providerData);
+    if (providerData.length < 2)
+      return -1;
+    if(addTreeData(1, providerData[1], toAdd) == null)
+      return -1;
+    return 1;
+  }
+
+  public int removeProvider(String providerID) {
+    if(removeTreeData(1,providerID) == null)
+      return -1;
+    return 1;
+  }
+
+  // should return Provider object
+  public Provider searchProvider(String query) {
+    try {
+      return (Provider) findData(1, query);
     }
-
-    // should return Provider object
-    public Provider searchProvider(String query) {
-        try {
-            return (Provider) findData(1, query);
-        }
-        catch (ClassCastException a) {
-            return null;
-        }
+    catch (ClassCastException a) {
+      return null;
     }
+  }
 
-    public void displayCurrentServices() {
-        try {
-            Provider current = (Provider) currentUser;
-            current.displayServices();
-        }
-        catch (ClassCastException a) {
-            System.out.println("Current user is not a Provider. Cannot display services");
-        }
+  public void displayCurrentServices() {
+    try {
+      Provider current = (Provider) currentUser;
+      current.displayServices();
     }
-
-    // Manage services
-    public int addService(String[] serviceData) {
-        Service toAdd = new Service(serviceData);
-        if(addTreeData(3, serviceData[1], toAdd) == null)
-            return -1;
-        return 1;
+    catch (ClassCastException a) {
+      System.out.println("Current user is not a Provider. Cannot display services");
     }
+  }
 
-    public int removeService(String serviceID) {
-        if(removeTreeData(3, serviceID) == null)
-            return -1;
-        return 1;
+  // Manage services
+  public int addService(String[] serviceData) {
+    Service toAdd = new Service(serviceData);
+    if(addTreeData(3, serviceData[1], toAdd) == null)
+      return -1;
+    return 1;
+  }
+
+  public int removeService(String serviceID) {
+    if(removeTreeData(3, serviceID) == null)
+      return -1;
+    return 1;
+  }
+
+
+  public Service searchService(String query) {
+    try {
+      return (Service) findData(2, query);
     }
-
-
-    public Service searchService(String query) {
-        try {
-            return (Service) findData(2, query);
-        }
-        catch (ClassCastException a)
-        {
-            return null;
-        }
+    catch (ClassCastException a)
+    {
+      return null;
     }
+  }
 
-    public int genMemberReport(int memberID) {
-        return 0;
-    }
+  public int genMemberReport(int memberID) {
+    return 0;
+  }
 
-    public int genProviderReport(int memberID) {
-        return 0;
-    }
+  public int genProviderReport(int memberID) {
+    return 0;
+  }
 
-    public int genAllMemberReports() {
-        return 0;
-    }
+  public int genAllMemberReports() {
+    return 0;
+  }
 
-    public int genAllProvidersReports() {
-        return 0;
-    }
+  public int genAllProvidersReports() {
+    return 0;
+  }
 
-    public int sendReports(String input) {
-        return 0;
-    }
+  public int sendReports(String input) {
+    return 0;
+  }
 
 
 
-    public int generateServiceRecord() {
-        return 0;
-    }
+  public int generateServiceRecord() {
+    return 0;
+  }
 
-    public int requestDirectory() {
-        return 0;
-    }
+  public int requestDirectory() {
+    return 0;
+  }
 
-    public int generateBill() {
-        return 0;
-    }
+  public int generateBill() {
+    return 0;
+  }
 
 
 }
--- src/com/Overlord/OverlordTest.java
+++ src/com/Overlord/OverlordTest.java
@@ -9,182 +9,182 @@ import java.util.Arrays;
 
 class OverlordTest {
 
-    @org.junit.jupiter.api.Test
-    void login() {
-        Overlord overlord = new Overlord();
-        String[] line;
+  @org.junit.jupiter.api.Test
+  void login() {
+    Overlord overlord = new Overlord();
+    String[] line;
 
-        try {
-            CSVReader reader = new CSVReader("tests/loginTests.csv");
+    try {
+      CSVReader reader = new CSVReader("tests/loginTests.csv");
 
-            while ((line = reader.getLine()) != null) {
-                assertEquals(Integer.parseInt(line[0]), overlord.login(Integer.parseInt(line[1]),line[2]), line[3]);
-            }
-            //assertEquals(-1, overlord.login(0, "000000"), "login with null reports failure");
-            //assertEquals(1, overlord.login(1, "000001"), "login with provider reports 1");
-            //assertEquals(2, overlord.login(2, "999999"), "login with provider reports 1");
+      while ((line = reader.getLine()) != null) {
+        assertEquals(Integer.parseInt(line[0]), overlord.login(Integer.parseInt(line[1]),line[2]), line[3]);
+      }
+      //assertEquals(-1, overlord.login(0, "000000"), "login with null reports failure");
+      //assertEquals(1, overlord.login(1, "000001"), "login with provider reports 1");
+      //assertEquals(2, overlord.login(2, "999999"), "login with provider reports 1");
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
     }
 
-    @org.junit.jupiter.api.Test
-    void logout() {
-    }
 
-    @org.junit.jupiter.api.Test
-    void memberCheckIn() {
-    }
+  }
 
-    @org.junit.jupiter.api.Test
-    void memberCheckOut() {
-    }
+  @org.junit.jupiter.api.Test
+  void logout() {
+  }
 
-    @org.junit.jupiter.api.Test
-    void addMember() {
-    }
+  @org.junit.jupiter.api.Test
+  void memberCheckIn() {
+  }
 
-    @org.junit.jupiter.api.Test
-    void removeMember() {
-    }
+  @org.junit.jupiter.api.Test
+  void memberCheckOut() {
+  }
 
-    @org.junit.jupiter.api.Test
-    void suspendMember() {
-    }
+  @org.junit.jupiter.api.Test
+  void addMember() {
+  }
 
-    @org.junit.jupiter.api.Test
-    void renewMember() {
-    }
+  @org.junit.jupiter.api.Test
+  void removeMember() {
+  }
 
-    @org.junit.jupiter.api.Test
-    void searchMember() {
-    }
+  @org.junit.jupiter.api.Test
+  void suspendMember() {
+  }
 
-    @org.junit.jupiter.api.Test
-    void addProvider() {
-        Overlord overlord = new Overlord();
-        String[] testArray = new String[7];
-        Arrays.fill(testArray, "");
+  @org.junit.jupiter.api.Test
+  void renewMember() {
+  }
 
-        assertEquals(-1, overlord.addProvider(testArray), "add member fails on null");
-        assertEquals(1, overlord.addProvider(testArray), "add member fails on null");
-    }
+  @org.junit.jupiter.api.Test
+  void searchMember() {
+  }
 
-    @org.junit.jupiter.api.Test
-    void removeProvider() {
-    }
+  @org.junit.jupiter.api.Test
+  void addProvider() {
+    Overlord overlord = new Overlord();
+    String[] testArray = new String[7];
+    Arrays.fill(testArray, "");
 
-    @org.junit.jupiter.api.Test
-    void searchProvider() {
-    }
+    assertEquals(-1, overlord.addProvider(testArray), "add member fails on null");
+    assertEquals(1, overlord.addProvider(testArray), "add member fails on null");
+  }
 
-    @org.junit.jupiter.api.Test
-    void addService() {
-    }
+  @org.junit.jupiter.api.Test
+  void removeProvider() {
+  }
 
-    @org.junit.jupiter.api.Test
-    void removeService() {
-    }
+  @org.junit.jupiter.api.Test
+  void searchProvider() {
+  }
 
-    @org.junit.jupiter.api.Test
-    void searchService() {
-    }
+  @org.junit.jupiter.api.Test
+  void addService() {
+  }
 
-    @org.junit.jupiter.api.Test
-    void genMemberReport() {
-    }
+  @org.junit.jupiter.api.Test
+  void removeService() {
+  }
 
-    @org.junit.jupiter.api.Test
-    void genProviderReport() {
-    }
+  @org.junit.jupiter.api.Test
+  void searchService() {
+  }
 
-    @org.junit.jupiter.api.Test
-    void genAllMemberReports() {
-    }
+  @org.junit.jupiter.api.Test
+  void genMemberReport() {
+  }
 
-    @org.junit.jupiter.api.Test
-    void genAllProvidersReports() {
-    }
+  @org.junit.jupiter.api.Test
+  void genProviderReport() {
+  }
 
-    @org.junit.jupiter.api.Test
-    void sendReports() {
-    }
+  @org.junit.jupiter.api.Test
+  void genAllMemberReports() {
+  }
 
-    @org.junit.jupiter.api.Test
-    void generateServiceRecord() {
-    }
+  @org.junit.jupiter.api.Test
+  void genAllProvidersReports() {
+  }
 
-    @org.junit.jupiter.api.Test
-    void requestDirectory() {
-    }
+  @org.junit.jupiter.api.Test
+  void sendReports() {
+  }
 
-    @org.junit.jupiter.api.Test
-    void generateBill() {
-    }
+  @org.junit.jupiter.api.Test
+  void generateServiceRecord() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void requestDirectory() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void generateBill() {
+  }
 }
 
 class CSVReader {
+  /*
+   *  Class to read CSV data from .csv files
+   */
+
+  private String fileName;
+  private BufferedReader reader;
+
+  CSVReader(String newFile) throws FileNotFoundException {
     /*
-     *  Class to read CSV data from .csv files
+     *  Overloaded constructor
+     *
+     *  Input:
+     *  String newFile: file to try to be opened
+     *  Throws:
+     *  FileNotFoundException: file can not be opened properly
      */
 
-    private String fileName;
-    private BufferedReader reader;
+    this.fileName = newFile;
+    this.reader = setFile(newFile);
+  }
 
-    CSVReader(String newFile) throws FileNotFoundException {
-        /*
-         *  Overloaded constructor
-         *
-         *  Input:
-         *  String newFile: file to try to be opened
-         *  Throws:
-         *  FileNotFoundException: file can not be opened properly
-         */
+  private BufferedReader setFile(String newFile) throws FileNotFoundException {
+    /*
+     *  Returns BufferedReader object based on file
+     *
+     *  Input:
+     *  String newFile: file to try to be opened
+     *  Throws:
+     *  FileNotFoundException: file can not be opened properly
+     *  Output:
+     *  Returns BufferedReader object pointing to file if opened properly
+     */
 
-        this.fileName = newFile;
-        this.reader = setFile(newFile);
+    return reader = new BufferedReader(new FileReader(newFile));
+  }
+
+  String[] getLine() {
+    /*
+     *  Returns current line in CSV file
+     *
+     *  Output:
+     *  String[]: Returns current line in CSV as array of String objects
+     */
+
+    //  Returns String array of current line, already broken up by deliminator ","
+    //  In case of readLine() exception
+    try {
+      //  Read next line, check if eof
+      String currentLine;
+      if ((currentLine = reader.readLine()) == null)
+        return null;
+
+      return currentLine.split(",");
     }
-
-    private BufferedReader setFile(String newFile) throws FileNotFoundException {
-        /*
-         *  Returns BufferedReader object based on file
-         *
-         *  Input:
-         *  String newFile: file to try to be opened
-         *  Throws:
-         *  FileNotFoundException: file can not be opened properly
-         *  Output:
-         *  Returns BufferedReader object pointing to file if opened properly
-         */
-
-        return reader = new BufferedReader(new FileReader(newFile));
+    //  Catch IOException
+    catch (IOException a) {
+      System.out.println("Could not read line from " + fileName);
+      return null;
     }
-
-    String[] getLine() {
-        /*
-         *  Returns current line in CSV file
-         *
-         *  Output:
-         *  String[]: Returns current line in CSV as array of String objects
-         */
-
-        //  Returns String array of current line, already broken up by deliminator ","
-        //  In case of readLine() exception
-        try {
-            //  Read next line, check if eof
-            String currentLine;
-            if ((currentLine = reader.readLine()) == null)
-                return null;
-
-            return currentLine.split(",");
-        }
-        //  Catch IOException
-        catch (IOException a) {
-            System.out.println("Could not read line from " + fileName);
-            return null;
-        }
-    }
+  }
 }
