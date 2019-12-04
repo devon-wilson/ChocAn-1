4d5c5f7053aa6acf1f594a759f59203eaf79495c
ThomasMLawrence <tmlawrence54@gmail.com>
2019-12-04 13:10:38 -0800

    added more tests and passed more tests

--- src/com/DataClasses/UserTest.java
+++ src/com/DataClasses/UserTest.java
@@ -56,34 +56,110 @@ class UserTest {
 
     @org.junit.jupiter.api.Test
     void changeName() {
+        Member myMember = new Member();
+        String toChange = null;
+        if(myMember.changeName(toChange) != -1){
+            fail("Change name to null failed");
+        }
+        String toChange = new String("Test");
+        if(myMember.changeName(toChange) != 0){
+            fail("Valid name change failed");
+        }
+        String [] components = myMember.getAll();
+        if(components[0].equals(toChange) == false){
+            fail("Name does not match");
+        }
     }
 
     @org.junit.jupiter.api.Test
     void changeID() {
+        Member myMember = new Member();
+        String toChange = null;
+        if(myMember.changeID(toChange) != -1){
+            fail("Change ID to null failed");
+        }
+        String toChange = new String("Test");
+        if(myMember.changeID(toChange) != 0){
+            fail("Valid ID change failed");
+        }
+        String [] components = myMember.getAll();
+        if(components[1].equals(toChange) == false){
+            fail("ID does not match");
+        }
     }
 
     @org.junit.jupiter.api.Test
     void changeAddress() {
+        Member myMember = new Member();
+        String toChange = null;
+        if(myMember.changeAddress(toChange) != -1){
+            fail("Change address to null failed");
+        }
+        String toChange = new String("Test");
+        if(myMember.changeAdress(toChange) != 0){
+            fail("Valid adress change failed");
+        }
+        String [] components = myMember.getAll();
+        if(components[2].equals(toChange) == false){
+            fail("Adress does not match");
+        }
     }
 
     @org.junit.jupiter.api.Test
     void changeCity() {
+        Member myMember = new Member();
+        String toChange = null;
+        if(myMember.changeCity(toChange) != -1){
+            fail("Change City to null failed");
+        }
+        String toChange = new String("Test");
+        if(myMember.changeCity(toChange) != 0){
+            fail("Valid city change failed");
+        }
+        String [] components = myMember.getAll();
+        if(components[3].equals(toChange) == false){
+            fail("City does not match");
+        }
     }
 
     @org.junit.jupiter.api.Test
     void changeState() {
+        Member myMember = new Member();
+        String toChange = null;
+        if(myMember.changeState(toChange) != -1){
+            fail("Change state to null failed");
+        }
+        String toChange = new String("Test");
+        if(myMember.changeState(toChange) != 0){
+            fail("Valid state change failed");
+        }
+        String [] components = myMember.getAll();
+        if(components[4].equals(toChange) == false){
+            fail("State does not match");
+        }
     }
 
     @org.junit.jupiter.api.Test
     void changeZip() {
+        Member myMember = new Member();
+        String toChange = null;
+        if(myMember.changeZip(toChange) != -1){
+            fail("Change zip to null failed");
+        }
+        String toChange = new String("Test");
+        if(myMember.changeZip(toChange) != 0){
+            fail("Valid zip change failed");
+        }
+        String [] components = myMember.getAll();
+        if(components[5].equals(toChange) == false){
+            fail("Zip does not match");
+        }
     }
 
-    @org.junit.jupiter.api.Test
-    void getAll() {
-    }
 
     @org.junit.jupiter.api.Test
     void get() {
+        
     }
 
     private int buildUser(String [] comp1, String [] comp2){
