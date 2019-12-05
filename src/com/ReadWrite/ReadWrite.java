6e73aeac7a4550721b8877f5485d213f8826c701
Jason <Jason.Forral@gmail.com>
2019-12-04 17:51:39 -0800

    updated ReadWrite class and dependent exception handling

--- src/com/ReadWrite/ReadWrite.java
+++ src/com/ReadWrite/ReadWrite.java
@@ -5,60 +5,118 @@ import java.util.ArrayList;
 
 public class ReadWrite
 {
-
-    public String[] fileRead(String filename)
+    public static String[] fileRead(String filename) throws IOException
     {
-        try {
-            BufferedReader file = new BufferedReader(new FileReader(filename));
-            ArrayList<String> input = new ArrayList<>();
-            String line;
+        FileReader file;
+        ArrayList<String> input = new ArrayList<String>();
+        String[] data;
+        int i = 0;
+        int c;
 
-            while ((line = file.readLine()) != null) {
-                input.add(line);
-            }
-            file.close();
-
-            int size = input.size();
-            String[] data = new String[size];
-
-            for (int i = 0; i < size; ++i)
-                data[i] = input.get(i);
-
-            return data;
+        try
+        {
+            file = new FileReader(filename);
         }
-        catch (IOException a) {
-            System.out.println("Could not open " + filename);
+        catch(FileNotFoundException e)
+        {
+            System.out.println("File not found.");
             return null;
         }
+
+        input.add("");
+        while((c = file.read()) != -1)
+        {
+            if (c == ',')
+            {
+                input.add("");
+                ++i;
+            } else
+                input.set(i, input.get(i) + (char) c);
+        }
+        file.close();
+
+        c = input.size();
+        data = new String[c];
+        for(i = 0; i < c; ++i)
+            data[i] = input.get(i);
+
+        return data;
     }
 
-    static String[][] fileReadAll(String filename) throws IOException
+    public static String[][] fileReadAll(String filename) throws IOException
     {
-        String[][] ret = {{"Placeholder"}, {"Placeholder"}};
+        FileReader file;
+        ArrayList<String> input = new ArrayList<String>();
+        ArrayList<String[]> holder = new ArrayList<String[]>();
+        String[][] ret;
+        String[] data;
+        int i = 0;
+        int j = 0;
+        int c;
+
+        try
+        {
+            file = new FileReader(filename);
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.println("File not found.");
+            return null;
+        }
+
+        input.add("");
+        while((c = file.read()) != -1)
+        {
+            if(c == ',')
+            {
+                input.add("");
+                ++i;
+            }
+            else if(c == '\n')
+            {
+                holder.add(new String[input.size()]);
+                data = new String[input.size()];
+                for(i = 0; i < input.size(); ++i)
+                    data[i] = input.get(i);
+                holder.set(j, data);
+                input.clear();
+                input.add("");
+                ++j;
+                i = 0;
+            }
+            else
+                input.set(i, input.get(i) + (char) c);
+        }
+
+        ret = new String[holder.size()][];
+        for(i = 0; i < holder.size(); ++i)
+        {
+            ret[i] = new String[holder.get(i).length];
+            ret[i] = holder.get(i);
+        }
+
         return ret;
     }
 
-    static int fileWrite(String filename, String[] data, boolean append)
+    public static int fileWrite(String filename, String[] data, boolean append) throws IOException
     {
+        FileWriter file;
+        int i;
+
         if(data[0] == null)
             return -1;
 
-        try {
-            FileWriter file = new FileWriter(filename + ".txt", append);
-            int i;
-            for (i = 0; i < data.length - 1; ++i) {
-                file.write(data[i]);
-                file.write(',');
-            }
+        file = new FileWriter(filename, append);
+        for(i = 0; i < data.length - 1; ++i)
+        {
             file.write(data[i]);
-            file.write('\n');
+            if(i != data.length - 1)
+                file.write(',');
+        }
+        file.write(data[i]);
+        file.write('\n');
 
-            file.close();
-            return 1;
-        }
-        catch (IOException a) {
-            System.out.println("Could not open " + filename);
-            return -1;
-        }
+        file.close();
+        return 1;
     }
 }
